2848314eb125de07b108dc820dd41b87558b8a98
Owen Klan <owen.klan@equatetechnologies.com.au>
2021-10-20 19:27:09 +1000

    Set max height for top-row option groups
    This makes things nicer if you maximise the window. Only the
    main placement area expands to take up as much space as possible.
    The horizontal top controls will exapand horizontally, though.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -79,6 +79,7 @@ void MainWindow::setupMenusAndActions() {
 
 void MainWindow::setupModeSelectGroup() {
     modeSelectGroup = new QGroupBox(tr("Mode Select"));
+    modeSelectGroup->setMaximumHeight(100);
     connectModeButton = new QPushButton("C&onnect");
     connectModeButton->setObjectName("connectModeButton");
     connectModeButton->setToolTip("Change to 'Connect' mode");
@@ -108,6 +109,7 @@ void MainWindow::setupModeSelectGroup() {
 
 void MainWindow::setupObjectSelectGroup(QWidget *parent = nullptr) {
     objectSelectGroup = new QGroupBox(tr("Object Select"));
+    objectSelectGroup->setMaximumHeight(100);
     objectSelectorCombo = new QComboBox(parent);
     objectSelectorCombo->setMinimumWidth(150);
     objectSelectorCombo->setMaximumWidth(250);
